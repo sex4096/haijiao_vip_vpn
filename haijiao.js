0c2b6938149aae6e28bb8660b3ff6a564c2c8d0e
John Doe <johndoe@example.com>
2024-04-30 17:58:42 +0800

    更新仓库分支

--- haijiao.js
+++ haijiao.js
@@ -18,7 +18,7 @@ const dependencies = [
   "https://cdn.jsdelivr.net/npm/antd@5.16.4/dist/antd.min.js",
   "https://cdn.jsdelivr.net/npm/@ant-design/icons@5.3.6/dist/index.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@ant-design/icons@5.3.6/dist/index.umd.min.js",
-  "https://cdn.jsdelivr.net/gh/sex4096/haijiao_vip/haijiao.js"
+  "https://cdn.jsdelivr.net/gh/sex4096/haijiao_vip@master/haijiao.js"
 ];
 // 在返回页面中添加js
 let body = $response.body;
